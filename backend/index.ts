aa66b4dda7661ca1ff18f492dfc47772ddd7c839
Davijo <danvj93@gmail.com>
2025-10-14 13:00:44 +0200

    Hello backend world!

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on http://localhost:8000");
+});
