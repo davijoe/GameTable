70ddfdc0dbb6caf2935353cadd546683f23ee0dc
Asmuse <asbot@live.dk>
2025-10-30 15:47:48 +0100

    finished artists endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.controller.game_controller import router as game_router
+from app.controller.artist_controller import router as artist_router
 
 app = FastAPI(title="Game API")
 
 app.include_router(game_router)
+app.include_router(artist_router)
 
 # Optional health endpoint
 
