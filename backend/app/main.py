052758c7a716a426fa4ab6927a5c39b007333039
Asmuse <asbot@live.dk>
2025-10-30 12:51:02 +0100

    setup user endpoint - still missing code to hash password

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.controller.game_controller import router as game_router
+from app.controller.user_controller import router as user_router
 
 app = FastAPI(title="Game API")
 
 app.include_router(game_router)
+app.include_router(user_router)
 
 # Optional health endpoint
 
