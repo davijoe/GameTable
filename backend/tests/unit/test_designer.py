247c98582350ebae722644ba18b57cde77a323fc
Asmuse <asbot@live.dk>
2025-12-08 23:58:48 +0100

    dob tests for designer

--- backend/tests/unit/test_designer.py
+++ backend/tests/unit/test_designer.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 import pytest
 from pydantic import ValidationError
 
@@ -37,3 +39,45 @@ class TestDesignerName:
         """Test that valid name values are accepted."""
         designer = DesignerCreate(name=name)
         assert designer.name == name
+
+
+class TestDesignerDob:
+    @pytest.mark.parametrize(
+        "dob",
+        [
+            date(1, 1, 1),
+            date(1, 1, 2),
+            date(1000, 6, 15),
+            date(1899, 12, 30),
+            date(1899, 12, 31),
+            date.today() + timedelta(days=1), # small risk it could be valid if test runs at midnight
+            date.today() + timedelta(days=2),
+            date.today() + timedelta(days=60 * 365),
+            "0000-00-00",
+            "2000-00-05",
+            "2005-03-00",
+            "15-15-1990",
+            "2000-13-10",
+            "1980-04-31",
+            "1900-02-29",
+        ],
+    )
+    def test_invalid_dob(self, dob):
+        with pytest.raises(ValidationError):
+            DesignerCreate(name="Valid Name", dob=dob)
+
+    @pytest.mark.parametrize(
+        "dob",
+        [
+            date(1900, 1, 1),
+            date(1900, 1, 2),
+            date(1960, 12, 31),
+            date.today() - timedelta(days=1),
+            date.today(),
+            "2000-02-29",
+        ],
+    )
+    def test_valid_dob(self, dob):
+        designer = DesignerCreate(name="Valid Name", dob=dob)
+        expected = dob if isinstance(dob, date) else date.fromisoformat(dob)
+        assert designer.dob == expected
