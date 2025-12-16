312d8641dc7e5fc3337aebe208b51b62c35fa15e
Nademtis <mathias.99.madsen@gmail.com>
2025-12-16 19:18:41 +0100

    react query dev tools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import theme from "./theme";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient({
@@ -27,6 +28,7 @@ createRoot(document.getElementById("root")!).render(
           <App />
         </BrowserRouter>
       </ChakraProvider>
+      <ReactQueryDevtools/>
     </QueryClientProvider>
   </StrictMode>
 );
