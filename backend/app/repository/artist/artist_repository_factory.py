1eaa2b8886614959424a9f829a224b592f2ab0b9
Nademtis <mathias.99.madsen@gmail.com>
2026-01-03 13:30:24 +0100

    added empty factory files

---
+++ backend/app/repository/artist/artist_repository_factory.py
@@ -0,0 +1,21 @@
+import os
+
+from app.utility.db_mongo import get_db as get_mongo_db
+from app.utility.db_neo import get_neo
+from app.utility.db_sql import get_sql_db
+
+DB_MODE = os.getenv("DB_MODE", "sql").lower()
+
+
+def get_artist_repository():
+    if DB_MODE == "sql":
+        db = next(get_sql_db())
+        return (db)
+    elif DB_MODE == "mongo":
+        db = next(get_mongo_db())
+        return (db)
+    elif DB_MODE == "neo":
+        db = get_neo()
+        return (db)
+
+    raise ValueError(f"Unknown DB_MODE: {DB_MODE}")
